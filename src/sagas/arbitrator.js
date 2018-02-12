3117f08a8b202f1e3be8dc67ffea729373bb6f6b
epiqueras <epiquerass@gmail.com>
2018-02-12 14:48:03 -0800

    feat(arbitrator): activate PNK action and saga

--- src/sagas/arbitrator.js
+++ src/sagas/arbitrator.js
@@ -59,6 +59,34 @@ function* buyPNK({ payload: { amount } }) {
 }
 
 /**
+ * Activates PNK for the current wallet.
+ */
+function* activatePNK({ payload: { amount } }) {
+  try {
+    yield put(action(arbitratorActions.PNKBalance.UPDATE))
+
+    const { tokenBalance, activatedTokens, lockedTokens } = yield call(
+      kleros.arbitrator.activatePNK,
+      amount,
+      ARBITRATOR_ADDRESS,
+      yield select(walletSelectors.getAccount)
+    )
+
+    yield put(
+      action(arbitratorActions.PNKBalance.RECEIVE_UPDATED, {
+        PNKBalance: {
+          tokenBalance: Number(tokenBalance), // TODO: Fix on API side
+          activatedTokens,
+          lockedTokens: Number(lockedTokens) // TODO: Fix on API side
+        }
+      })
+    )
+  } catch (err) {
+    yield put(errorAction(arbitratorActions.PNKBalance.FAIL_UPDATE, err))
+  }
+}
+
+/**
  * Fetches the arbitrator's data.
  */
 function* fetchArbitratorData() {
@@ -106,6 +134,7 @@ export default function* arbitratorSaga() {
   // PNK Balance
   yield takeLatest(arbitratorActions.PNKBalance.FETCH, fetchPNKBalance)
   yield takeLatest(arbitratorActions.PNKBalance.BUY, buyPNK)
+  yield takeLatest(arbitratorActions.PNKBalance.ACTIVATE, activatePNK)
 
   // Arbitrator Data
   yield takeLatest(arbitratorActions.arbitratorData.FETCH, fetchArbitratorData)
