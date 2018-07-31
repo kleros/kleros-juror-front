b3bfbc7f8637820fce41cd4a91f32e21eb97d139
epiqueras <epiquerass@gmail.com>
2018-07-31 10:30:41 -0500

    feat(arbitrator): activation => deposit

--- src/constants/arbitrator.js
+++ src/constants/arbitrator.js
@@ -1,5 +1,5 @@
 export const PERIOD_ENUM = [
-  'activation', // When juror can activate their tokens and parties give evidences
+  'deposit', // When juror can activate their tokens and parties give evidences
   'draw', // When jurors are drawn at random, note that this period is fast
   'vote', // Where jurors can vote on disputes
   'appeal', // When parties can appeal the rulings
