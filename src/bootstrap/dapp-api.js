24525edfc304117c102bd2b65373ebf54530cd17
Sam Vitello <sam.j.vitello@gmail.com>
2018-11-02 22:32:25 +0100

    fix: web3 needs current provider

--- src/bootstrap/dapp-api.js
+++ src/bootstrap/dapp-api.js
@@ -11,7 +11,8 @@ let eth
 if (process.env.NODE_ENV === 'test')
   eth = new Eth(require('ganache-cli').provider())
 else if (window.ethereum) eth = new Eth(window.ethereum)
-else if (window.web3) eth = new Eth(window.web3)
+else if (window.web3 && window.web3.currentProvider)
+  eth = new Eth(window.web3.currentProvider)
 else eth = new Eth(new Eth.HttpProvider(ETHEREUM_PROVIDER))
 
 let ARBITRATOR_ADDRESS
